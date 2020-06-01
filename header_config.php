9e571b37ee322f34c90f7143b2ff01f21919189d
won seung hun <dighqnqk1@gmail.com>
2020-06-01 17:06:17 +0900

    수정

--- header_config.php
+++ header_config.php
@@ -1,6 +1,6 @@
 <?php
 include_once( $_SERVER["DOCUMENT_ROOT"]."/header_config_json.php");
-
+   
 ?>
 <html>
 <head>
@@ -10,4 +10,5 @@ include_once( $_SERVER["DOCUMENT_ROOT"]."/header_config_json.php");
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script src="/js/jquery-1.11.3.min.js?v=System.currentTimeMillis()"></script>
 <script src="/js/common.js?v=System.currentTimeMillis()"></script>
+<script src="/js/textData.js"></script>
 </head>
