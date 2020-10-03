abfc9f45f707276a94820865c89a1102d7470127
Sihwa Park <astrayne@naver.com>
2020-10-03 23:42:57 +0900

    User Join Step3 Finished

--- join/step3.php
+++ join/step3.php
@@ -1,10 +1,31 @@
 <?php
 include_once( $_SERVER["DOCUMENT_ROOT"]."/header.php");
+include_once( $_SERVER["DOCUMENT_ROOT"]."/config/db_config.php");
+$sql = "SELECT id, name FROM countries";
+$countries = mysqli_query($conn, $sql);
 ?>
 <!--From pages/page10 html-->
 <!--Nationality Information-->
+<!--Array ( [userNm] => sihwa [firstNm] => sihwa [lastNm] => park [userPass] => sihwa [priChk] => on [userSex] => 1 )-->
 <html>
 <body>
+<script>
+    function formSubmit() {
+        var form = document.step3;
+        document.getElementById("userNm").value = "<?php echo $_POST['userNm'];?>"
+        document.getElementById("firstNm").value = "<?php echo $_POST['firstNm'];?>"
+        document.getElementById("lastNm").value = "<?php echo $_POST['lastNm'];?>"
+        document.getElementById("userPass").value = "<?php echo $_POST['userPass'];?>"
+        document.getElementById("priChk").value = "<?php echo $_POST['priChk'];?>"
+        document.getElementById("userSex").value = "<?php echo $_POST['userSex'];?>"
+        if( $("#uContry").val() === "" || $("#uContry").val() === undefined){
+            alert("Choose your nationality.");
+            return false;
+        }
+        form.submit();
+
+    }
+</script>
 <div class="body">
     <div class="container" id="container-menu">
         <?php include($_SERVER["DOCUMENT_ROOT"]."/menu.php"); ?>
@@ -19,24 +40,29 @@ include_once( $_SERVER["DOCUMENT_ROOT"]."/header.php");
                     <div class="title-div">
                         <span class="textDefault f36 bold"> Where are you from? </span>
                     </div>
+                    <form action="step4" method="post" name="step3">
+                        <input type="hidden" placeholder="Username" id="userNm" name="userNm">
+                        <input type="hidden" placeholder="Jane" id="firstNm" name="firstNm">
+                        <input type="hidden" placeholder="Doe" id="lastNm" name="lastNm">
+                        <input type="hidden" placeholder="Password" id="userPass" name="userPass">
+                        <input type="hidden" id="priChk" name="priChk">
+                        <input type="hidden" id="userSex" name="userSex">
 
-                    <div class="input-form">
-                        <div class="divBox10">
-                            <select class="select textDefault" name="uContry" id="uContry">
-                                <option value="">Select Country</option>
-                                <option value="1">Jersey</option>
-                                <option value="2">Kazakhstan</option>
-                                <option value="3">Kenya</option>
-                                <option value="4">Kiribati</option>
-                                <option value="5">Korea, Democratic People's Republic
-                                    of</option>
-                                <option value="6">Korea Republic of</option>
-                            </select>
+                        <div class="input-form">
+                            <div class="divBox10">
+                                <select class="select textDefault" name="uContry" id="uContry">
+                                    <option value="">Select Country</option>
+                                    <?php
+                                    foreach($countries as $country){
+                                        echo("<option value='".$country["id"]."'>".$country["name"]."</option>");
+                                    }
+                                    ?>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-
+                    </form>
                     <div class="next">
-                        <span class="textDefault bold" onClick="contry('page11.html')">Next</span>
+                        <span class="textDefault bold" onClick="formSubmit()">Next</span>
                     </div>
 
                 </div>
