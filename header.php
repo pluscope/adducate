e46193c950273ed4ae4e4d229eae71f786c3bb92
pluscope410@gmail.com <dureotkd410!@>
2020-10-19 21:24:35 +0900

    Add favicon

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     session_start();
 ?>
 <html>
-<title>Adducate Web App</title>
+<title>Adducate</title>
 <link href="/style.css" rel="stylesheet">
 <head>
     <meta charset="utf-8"></meta>
@@ -11,6 +11,7 @@
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="/js/jquery-1.11.3.min.js?v=System.currentTimeMillis()"></script>
     <script src="/js/common.js?v=System.currentTimeMillis()"></script>
-    <script src="/js/textData.js"></script>
+<!--    <script src="/js/textData.js"></script>-->
+    <link rel="icon" href="../img/favicon.ico"/>
 </head>
 </html>
