b84eeb653f5b56d7ed1562dac27e8725e552ad3a
pluscope410@gmail.com <dureotkd410!@>
2020-11-29 22:44:56 +0900

    etc

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="/js/jquery-1.11.3.min.js?v=System.currentTimeMillis()"></script>
     <script src="/js/common.js?v=System.currentTimeMillis()"></script>
-<!--    <script src="/js/textData.js"></script>-->
     <link rel="icon" href="../img/favicon.ico"/>
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
