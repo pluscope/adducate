22f3a3f2a29e4f5e12c4c833742757c2ec48087d
pluscope410@gmail.com <dureotkd410!@>
2021-01-28 17:28:26 +0900

    storybook lesson css fixed

--- class/storybook/story.php
+++ class/storybook/story.php
@@ -68,7 +68,7 @@ if($conn) {
                         <div class="pointer"><span onclick="location.href='/class/'" class="hover-green" style="cursor: pointer;">Class</span><span> > </span><span class="hover-green" onclick="location.href='/class/storybook/'" style="cursor: pointer;">Storybook</span><span> > <?php echo $storybook["title"] ?></span></div>
 
                         <div class="Lorem-text-overflow2">
-                            <div class="push title-div2" id="alivePush">
+                            <div class="pushStory title-div2" id="alivePush">
                                 <?php
                                     for($i=1; $i<=$total_lessons; ++$i){
                                         if($lesson_id == $lessons[$i-1]["id"]){
