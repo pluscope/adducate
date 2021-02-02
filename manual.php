f584e5f979d7fa53e7d33a5848ef028d3a99ea91
pluscope410@gmail.com <dureotkd410!@>
2021-02-02 16:57:37 +0900

    manual css & bugs fixed

--- manual.php
+++ manual.php
@@ -36,7 +36,7 @@ if($conn) {
                             $video_links = explode( ';', $row["video_link"]);
                             for($i=0; $i<count($contents); ++$i){
                                 echo "<div class=\"divBox5_2\">";
-                                echo "<iframe src='".$video_links[$i]."'>";
+                                echo "<iframe width=\"480\" height=\"260\" src='".$video_links[$i]."'>";
                                 echo "</iframe>";
                                 echo "<div class=\"textDefault\">";
                                 echo $contents[$i];
